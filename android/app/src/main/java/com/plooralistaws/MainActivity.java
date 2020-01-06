eaa0969e2f541cf4b6284040fe656aff6def7682
ofermalbin <ofer.malbin@gmail.com>
2020-01-06 10:05:35 +0200

    SplashActivity

--- android/app/src/main/java/com/plooralistaws/MainActivity.java
+++ android/app/src/main/java/com/plooralistaws/MainActivity.java
@@ -1,6 +1,8 @@
 package com.plooralistaws;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // import this
+import android.os.Bundle; // import this
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "Plooralist";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+  
 }
